d6a3e7694441c2e01b922d4d400b6a9480057334
LZD962 <73733581+LZD962@users.noreply.github.com>
2021-01-03 01:50:38 +0800

    squaring modify

--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -15,7 +15,7 @@ class SquareNumberApp(App):
 
     def build(self):
         """Build the Kivy app from the kv file."""
-        Window.size = (200, 100)
+        Window.size = (400, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
@@ -23,8 +23,11 @@ class SquareNumberApp(App):
     def handle_calculate(self, value):
         """Handle calculation (could be button press or other call),
         output result to label widget."""
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = int(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = ""
 
 
 SquareNumberApp().run()
