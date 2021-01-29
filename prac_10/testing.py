97df8a55ad3816f50053a27e6b570aa3396732b6
LZD962 <73733581+LZD962@users.noreply.github.com>
2021-01-29 14:59:39 +0800

    testing update

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -46,13 +46,15 @@ def run_tests():
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10
 
 
 run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
+
 
 # TODO: 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
@@ -66,3 +68,16 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+def capital(phrase):
+    """
+    starting with a capital and ending with a single full stop.
+    >>> capital('hello')
+    'Hello.'
+    >>> capital('It is an ex parrot.')
+    'It is an ex parrot.'
+    """
+    if phrase.endswith('.'):
+        return phrase.capitalize()
+    else:
+        return phrase.capitalize() + '.'
